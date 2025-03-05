1fdc25d38c303ba71a4e36019dc82a4cb1a280bb
Nataliia Valko <75252511+NataliiaValko@users.noreply.github.com>
2025-03-05 11:30:14 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import iziToast from 'izitoast';
+
+import { getImagesByQuery } from './js/pixabay-api';
+
+import 'izitoast/dist/css/iziToast.min.css';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
+
+const formEl = document.querySelector('.search-form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = Object.fromEntries(new FormData(event.target));
+
+  if (data.message === '') {
+    return;
+  }
+
+  showLoader();
+  clearGallery();
+
+  getImagesByQuery(data.message)
+    .then(({ hits: results }) => {
+      if (results.length === 0) {
+        iziToast.info({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      createGallery(results);
+    })
+    .catch(err => {
+      iziToast.error({
+        message: 'Error!!!',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
+
+formEl.addEventListener('submit', handleSubmit);
